4a78673ef5d90e736c92af0a86b66bb3522f3235
saideep pudale <saideeppudale@gmail.com>
2025-06-16 13:04:14 +0530

    changes

--- indexed.js
+++ indexed.js
@@ -1,9 +1,13 @@
 const age = 24;
 
-const name1 = "saideep";
+
 
 const date = '18/05/2004';
 
 function isAbove18() {
     return age>=18;
+}
+
+function newFn(){
+    console.log("my name is saideep");
 }
